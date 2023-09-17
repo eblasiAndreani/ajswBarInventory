e814646b51157997a79299efd29cf56f5bb957a8
Blasi Eduardo <eblasi@andreani.com>
2023-09-17 00:06:26 -0300

    Commit Initial

---
+++ src/main/java/com/ajsw/barInventory/BarInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.ajsw.barInventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajsw/barInventory/BarInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajsw.barInventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
